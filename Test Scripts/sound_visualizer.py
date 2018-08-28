c67ad541b7182f03e29de41c6f393f24e2375e75
Kevin Rosenberg <karsonic7283@gmail.com>
2018-08-28 18:47:21 +0000

    Updated script to Python3

--- Test Scripts/sound_visualizer.py
+++ Test Scripts/sound_visualizer.py
@@ -1,10 +1,11 @@
+import numpy as np
 import sounddevice as sd
 
 duration = 10 * 1000 # 10 seconds
 
 def print_sound(indata, outdata, frames, time, status):
     volume_norm = np.linalg.norm(indata) * 10
-    print "|" * int(volume_norm)
+    print("|" * int(volume_norm))
 
 with sd.Stream(callback=print_sound):
     sd.sleep(duration)
