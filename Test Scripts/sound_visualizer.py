2deb71757f60dd2d1ed366427bbb70f4497a0ce2
Kevin Rosenberg <karsonic7283@gmail.com>
2018-08-29 16:59:07 +0000

    Fixed input

--- Test Scripts/sound_visualizer.py
+++ Test Scripts/sound_visualizer.py
@@ -20,32 +20,34 @@ class SoundVisualizer:
     def visualize_print(self):
         """Visualize the values by printing them as bars to the terminal
         """
-        with sd.Stream(callback=this._print):
+        with sd.Stream(callback=self._print):
             sd.sleep(self.duration_sec * 1000)
 
     def _print(self, indata, *_):
         volume_norm = np.linalg.norm(indata) * self.scale
-        self._values.append(value_norm)
+        self._values.append(volume_norm)
         print(self.symbol * int(volume_norm))
 
 
-def _input_defaults(param_name: str, deafult_value, units: str = ""):
-    propmt = "Enter your {0} (leave blank for {1}{2}): ".format(
+def _input_defaults(param_name: str, default_value, units: str = ""):
+    prompt = "Enter your {0} (leave blank for {1}{2}): ".format(
         param_name, 
-        deafult_value, 
+        default_value, 
         units
     )
-    value_input = input(propmt)
-    return deafult_value if value_input != '' else value
+    # Need to print separately due to remote terminal issue
+    print(prompt)
+    value_input = input()
+    return value_input if value_input != '' else default_value
 
 
 def main():
-    scale = int(_input_defaults("scale", 10, "seconds"))
-    duration = int(_input_defaults("duration", 10, "seconds"))
+    scale = int(_input_defaults("scale", 10, "x"))
+    duration = int(_input_defaults("duration", 10, " seconds"))
     symbol = _input_defaults("symbol", "|")
 
     sound_visualizer = SoundVisualizer(scale, duration, symbol) 
-    sound_visualizer.visualize()
+    sound_visualizer.visualize_print()
 
 if __name__ == '__main__':
     main()
